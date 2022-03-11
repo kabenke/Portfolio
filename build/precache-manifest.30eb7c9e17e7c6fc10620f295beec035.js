80d60fd8afdfae5e64fec0e1e8a6435bd87fd79f
kabenke <kabenke_nana@yahoo.fr>
2022-03-11 02:45:45 +0100

    make -reponsivve --for --everyscreen-size

---
+++ build/precache-manifest.30eb7c9e17e7c6fc10620f295beec035.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b8285a38b30006834c8",
+    "url": "./static/css/main.2aec37a1.chunk.css"
+  },
+  {
+    "revision": "3b8285a38b30006834c8",
+    "url": "./static/js/main.3b8285a3.chunk.js"
+  },
+  {
+    "revision": "b80ca6f8302c7cc58d69",
+    "url": "./static/js/1.b80ca6f8.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "d3e36ba35781e58a8620bd37e2f12ee3",
+    "url": "./static/media/3.d3e36ba3.png"
+  },
+  {
+    "revision": "d44cf5dbe99dfebda6f3a2226eabbcf9",
+    "url": "./static/media/9.d44cf5db.png"
+  },
+  {
+    "revision": "9a0fc489d0db7cd35f2ba534b9bcd150",
+    "url": "./static/media/8.9a0fc489.png"
+  },
+  {
+    "revision": "084a5deeb23be7a7690f686a6f9c45de",
+    "url": "./static/media/7.084a5dee.png"
+  },
+  {
+    "revision": "89b993d852243eb7d3ba28d35fe77a5a",
+    "url": "./static/media/6.89b993d8.png"
+  },
+  {
+    "revision": "52b35d9bfbc599c959333251d415f6ec",
+    "url": "./static/media/5.52b35d9b.png"
+  },
+  {
+    "revision": "ff55f525da3d3d142413f4d4c095ed16",
+    "url": "./static/media/4.ff55f525.png"
+  },
+  {
+    "revision": "0794fbafc034aab236872e0f7dad12b0",
+    "url": "./static/media/1.0794fbaf.png"
+  },
+  {
+    "revision": "80fea0c11a9d0aed486b9b893125a62a",
+    "url": "./static/media/2.80fea0c1.png"
+  },
+  {
+    "revision": "4fbe1f6f6742e693d9a030e35e578065",
+    "url": "./static/media/phone.4fbe1f6f.png"
+  },
+  {
+    "revision": "a7d4e8ac1b9183cfb8ef9d942a377beb",
+    "url": "./static/media/email.a7d4e8ac.png"
+  },
+  {
+    "revision": "90add3939651ca834f5bdbdf17c653d0",
+    "url": "./static/media/sun.90add393.png"
+  },
+  {
+    "revision": "ad528846e5092ed6983f7d2445fa4fb0",
+    "url": "./static/media/moon.ad528846.png"
+  },
+  {
+    "revision": "b06b7a158af11bc32abc672e12f5e0ac",
+    "url": "./index.html"
+  }
+];
